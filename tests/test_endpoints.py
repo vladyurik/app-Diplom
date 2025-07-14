583e96732091771ca97da4e6f402cb9a3ed4d140
Uladzislau Yurevich <77672974+vladyurik@users.noreply.github.com>
2025-07-14 23:18:16 +0300

    Update test_endpoints.py

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -2,14 +2,14 @@ import requests
 
 
 def test_homepage_accessible():
-    response = requests.get('http://localhost:5000/')
+    response = requests.get('http://localhost:5001/')
     assert response.status_code == 200
     assert 'Калькулятор' in response.text  # Или любой ожидаемый текст
 
 
 def test_addition():
     response = requests.post(
-        'http://localhost:5000/',
+        'http://localhost:5001/',
         data={'a': '2', 'b': '3', 'op': 'add'}
     )
     assert response.status_code == 200
