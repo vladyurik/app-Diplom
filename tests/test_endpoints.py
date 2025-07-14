84e626e13b10fc0e21d319a83c127db7934ff2bb
Uladzislau Yurevich <77672974+vladyurik@users.noreply.github.com>
2025-07-14 22:13:18 +0300

    Create test_endpoints.py

---
+++ tests/test_endpoints.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def test_homepage_accessible():
+    response = requests.get('http://localhost:5000/')
+    assert response.status_code == 200
+    assert 'Калькулятор' in response.text  # Или любой ожидаемый текст
+
+
+def test_addition():
+    response = requests.post('http://localhost:5000/', data={'a': '2', 'b': '3', 'op': 'add'})
+    assert response.status_code == 200
+    assert '5' in response.text
